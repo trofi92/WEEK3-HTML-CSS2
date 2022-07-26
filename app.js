dfc94c5f82bda4139f5d74d07c0aff53d78313b2
trofi92 <84131398+trofi92@users.noreply.github.com>
2022-07-26 16:18:47 +0900

    commit

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ function handleToLogin() {
     document.getElementsByClassName("to-login");
     addEventListener("click", alert("회원가입 페이지로 이동합니다!"));
 }
+
+
+
+
+
+function handleVisible() {
+    const visible = document.getElementById("rating"); 
+    if (rating.style.visibility !== "hidden") {
+        rating.style.visibility = "hidden";
+    }
+    else {
+        rating.style.visibility = "visible";
+    }
+}
+
+console.log(rating)
