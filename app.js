ffccda1e9a543fe76aee136643f1668a73cfcb8f
trofi92 <84131398+trofi92@users.noreply.github.com>
2022-07-26 11:45:56 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function handleSubmitInput(){
+    document.getElementsByClassName("submit");
+    addEventListener("click", alert("감사합니다!"));
+};
+
+
+function handleToLogin() {
+    document.getElementsByClassName("to-login");
+    addEventListener("click", alert("회원가입 페이지로 이동합니다!"));
+}
